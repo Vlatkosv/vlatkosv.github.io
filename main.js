bb75e917a421a220f2a8bb2187c2ef7c0fdf5413
VlatkosvPC <13476912+Vlatkosv@users.noreply.github.com>
2018-03-09 21:25:32 +1100

    update v0.4

--- main.js
+++ main.js
@@ -6,12 +6,10 @@ window.setInterval(function(){
 
 //autoload
 function checkSave(){
-  var savegame = JSON.parse(localStorage.getItem("save"));
 
-  if (typeof savegame.coins !== "undefined") coins = savegame.coins;
-      document.getElementById('coins').innerHTML = coins;
-  if (typeof savegame.BuildingOne !== "undefined") BuildingOne = savegame.BuildingOne;
-      document.getElementById('BuildingOne').innerHTML = BuildingOne;
+		loadButton();
+		//start the real-time timer
+		startTime();
 }
 
 //main currency
@@ -26,17 +24,18 @@ function coinsClick(number){
 
 // Building One's Code
 var BuildingOne = 0;
+var nextBuildingOneCost = 0;
 
 function buyBuildingOne(){
-    var costBuildingOne = Math.floor(10 * Math.pow(1.1,BuildingOne));     //works out the cost of this cursor
+    costBuildingOne = Math.floor(10 * Math.pow(1.1,BuildingOne));     //works out the cost of this cursor
     if(coins >= costBuildingOne){                                   //checks that the player can afford the cursor
         BuildingOne = BuildingOne + 1;                                   //increases number of cursors
     	coins = coins - costBuildingOne;                          //removes the coins spent
         document.getElementById('BuildingOne').innerHTML = BuildingOne;  //updates the number of cursors for the user
         document.getElementById('coins').innerHTML = coins;  //updates the number of coins for the user
     };
-    var nextCost = Math.floor(10 * Math.pow(1.1,BuildingOne));       //works out the cost of the next cursor
-    document.getElementById('costBuildingOne').innerHTML = nextCost;  //updates the cursor cost for the user
+    nextBuildingOneCost = Math.floor(10 * Math.pow(1.1,BuildingOne));       //works out the cost of the next cursor
+    document.getElementById('costBuildingOne').innerHTML = nextBuildingOneCost;  //updates the cursor cost for the user
 };
 
 //Autoclicker function
@@ -50,7 +49,8 @@ window.setInterval(function(){
 function saveButton(){
   var save = {
       coins: coins,
-      BuildingOne: BuildingOne
+      BuildingOne: BuildingOne,
+			nextBuildingOneCost: nextBuildingOneCost
   }
     		localStorage.setItem('save', JSON.stringify(save));
 }
@@ -63,9 +63,74 @@ if (typeof savegame.coins !== "undefined") coins = savegame.coins;
     document.getElementById('coins').innerHTML = coins;
 if (typeof savegame.BuildingOne !== "undefined") BuildingOne = savegame.BuildingOne;
     document.getElementById('BuildingOne').innerHTML = BuildingOne;
+
+if (typeof savegame.nextBuildingOneCost !== "undefined") nextBuildingOneCost = savegame.nextBuildingOneCost;
+		document.getElementById('costBuildingOne').innerHTML = nextBuildingOneCost;
 }
 
 //delete save button code coz yolo
 function delsaveButton(){
   localStorage.removeItem("save")
+}
+
+
+
+
+
+
+
+
+//<p id="timedif"></p>
+
+//set date for 1st of Jan 2018
+//var t1 = new Date("2018-01-01T12:00:00Z");
+//get current date and time
+//var t2 = new Date();
+//calculate the difference
+//var dif = t1.getTime() - t2.getTime();
+
+//TD = time difference between t1 and t2
+//turn the difference into minutes
+//var TD = dif / 60000;
+//tbd=time between dates
+//var TBD = Math.abs(TD);
+
+//document.getElementById("timedif").innerHTML = TBD;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//time
+
+var today = 0;
+var h = 0;
+var m = 0;
+var s = 0;
+var t = 0;
+
+function startTime() {
+    today = new Date();
+    h = today.getHours();
+    m = today.getMinutes();
+    s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+    h + ":" + m + ":" + s;
+    t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
 }
